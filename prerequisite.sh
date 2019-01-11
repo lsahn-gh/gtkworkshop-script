7bcfd5658942a5cdeb93bf9959bc28e50e80f493
Yi-Soo An <yisooan@gmail.com>
2019-01-11 20:36:09 +0900

    Edit prerequisite.sh

--- prerequisite.sh
+++ prerequisite.sh
@@ -2,26 +2,29 @@
 
 if [ -x "$(command -v flatpak)" ] && [ $(flatpak --version | cut -d" " -f2) != "1.0.6" ]; then
   echo ""
-  echo " ** Current flatpak is not 1.0.6"
-  echo " ** It will be purged and reinstall it"
+  echo " ** Current flatpak is not 1.0.6 **"
+  echo " ** It will be purged and reinstall it **"
+  echo ""
   sudo apt purge -y flatpak
 fi
 
 if ! [ -x "$(command -v flatpak)" ]; then
   echo ""
-  echo " ** Installing latest flatpak now..."
+  echo " ** Installing latest flatpak now... **"
+  echo ""
   sudo add-apt-repository ppa:alexlarsson/flatpak -y
   sudo apt update
   sudo apt install -y flatpak gnome-software-plugin-flatpak
 fi
 
-echo "Running remote-add command ..."
+echo ""
+echo " ** Running remote-add command ... **"
 flatpak remote-add --user flathub https://flathub.org/repo/flathub.flatpakrepo
 
-echo "Running remote-modify command ..."
+echo " ** Running remote-modify command ... **"
 flatpak remote-modify --user --collection-id=org.flathub.Stable flathub
 
-echo "Updating local repositories ..."
+echo " ** Updating local repositories ... **"
 flatpak update
 
 echo ""
